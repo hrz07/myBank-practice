563fa457c1fbe6c2c38a616ea82c63b3b5451904
hrz07 <hridoy.developer@yahoo.com>
2022-02-12 12:30:16 +0600

    login functionality added

---
+++ login.js
@@ -0,0 +1,13 @@
+document.getElementById('login-btn').addEventListener('click',()=>{
+    const email = "bank@gmail.com";
+    const pass = 1234;
+
+    const inputEmail = document.getElementById('email-input').value;
+    const inputPass = document.getElementById('pass-input').value;
+    document.getElementById('email-input').value = '';
+
+
+    if(email == inputEmail && pass == inputPass){
+        window.location.href="bank.html"
+    }
+})
