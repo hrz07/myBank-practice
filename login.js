e7e51fefe0029e53714b97eaaecd0101361caf23
hrz07 <hridoy.developer@yahoo.com>
2022-02-12 14:13:48 +0600

    bank design complete

--- login.js
+++ login.js
@@ -4,10 +4,11 @@ document.getElementById('login-btn').addEventListener('click',()=>{
 
     const inputEmail = document.getElementById('email-input').value;
     const inputPass = document.getElementById('pass-input').value;
-    document.getElementById('email-input').value = '';
 
+    document.getElementById('email-input').value='';
+    document.getElementById('pass-input').value='';
 
     if(email == inputEmail && pass == inputPass){
-        window.location.href="bank.html"
+        window.location.href="./bank.html"
     }
 })
